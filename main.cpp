2687ad64937dce7e27f96c881fdc1062c962838b
korin123 <makskornienkooriginal@gmail.com>
2026-03-16 01:25:45 +0200

    Added parallel non-blocking implementation

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ std::mutex mtx;
 int shared_count = 0;
 int shared_max = INT_MIN;
 
+std::atomic<int> atomic_count{0};
+std::atomic<int> atomic_max{INT_MIN};
+
 void run_sequential_test(const std::vector<int>& data) {
     int count = 0;
     int max_val = INT_MIN;
@@ -73,6 +76,48 @@ void run_mutex_test(const std::vector<int>& data, int num_threads) {
     std::cout << "Time: " << elapsed.count() << " ms" << std::endl;
 }
 
+void thread_worker_cas(const std::vector<int>& data, size_t start, size_t end) {
+    for (size_t i = start; i < end; ++i) {
+        if (data[i] % 3 == 0) {
+            int val = data[i];
+
+            int current_count = atomic_count.load();
+            while (!atomic_count.compare_exchange_weak(current_count, current_count + 1)) {}
+
+            int current_max = atomic_max.load();
+            while (val > current_max && !atomic_max.compare_exchange_weak(current_max, val)) {}
+        }
+    }
+}
+
+void run_cas_test(const std::vector<int>& data, int num_threads) {
+    atomic_count.store(0);
+    atomic_max.store(INT_MIN);
+    std::vector<std::thread> threads;
+    size_t chunk_size = data.size() / num_threads;
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < num_threads; ++i) {
+        size_t start = i * chunk_size;
+        size_t end = (i == num_threads - 1) ? data.size() : (i + 1) * chunk_size;
+        threads.emplace_back(thread_worker_cas, std::ref(data), start, end);
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> elapsed = end_time - start_time;
+
+    std::cout << "--- CAS Execution ---" << std::endl;
+    std::cout << "Threads used: " << num_threads << std::endl;
+    std::cout << "Count: " << atomic_count.load() << std::endl;
+    std::cout << "Maximum element: " << atomic_max.load() << std::endl;
+    std::cout << "Time: " << elapsed.count() << " ms" << std::endl;
+}
+
 int main() {
     const size_t size = 100000;
     std::vector<int> data(size);
@@ -87,6 +132,8 @@ int main() {
     run_sequential_test(data);
 
     run_mutex_test(data, num_threads);
+
+    run_cas_test(data, num_threads);
 
     return 0;
 }
